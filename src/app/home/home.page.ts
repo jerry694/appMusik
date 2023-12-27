c64ef422a81de9dc319e53da8305fb8ab0b34e88
jerry694 <wafoaristide@gmail.com>
2023-12-28 00:43:09 +0100

    v1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal!: IonModal ;
 
+  cards=[1,1,1,1,1,1,1,1,1,1]
+  loading=false
+  name!: string;
+  is_open=false
   constructor() {}
+  trash(){
+    this.cards=[]
+    this.loading=true
+  }
+  doRefresh(event: any): void {
+    this.trash()
+    setTimeout(() => {
+      this.cards=[1,1,1,1,1,1,1,1,1,1]
+      this.loading=false
+      event.detail.complete();
+    }, 2000);
+  }
 
+open(){
+  
+  this.is_open=true
+}
+  cancel() {
+    this.is_open=false
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.is_open=false
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    this.is_open=false
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      alert("hello");
+    }
+  }
 }
